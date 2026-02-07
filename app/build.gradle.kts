2ce29f2e47972f0bb2403ff0150b1017a85213bc
Aditya <aditya4grawal@gmail.com>
2026-02-07 14:36:15 +0530

    working map module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.OCEN"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.OCEN"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
